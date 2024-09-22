b1ac5a5cee9986c60a5a26eaa48740903c95cefa
yvssaratchandra <saratchandra.yellapragada@tutanota.com>
2024-09-22 19:12:41 +0200

    reformat lsh.c file and add gitignore

--- code/lsh.c
+++ code/lsh.c
@@ -34,109 +34,101 @@
 #include <fcntl.h>
 
 static void print_cmd(Command *cmd);
+
 static void print_pgm(Pgm *p);
+
 void stripwhite(char *);
 
-int main(void)
-{
-  for (;;)
-  {
-    char *line;
-    line = readline("> ");
-    if (line == NULL) {
-	    return 0;
-    }
-
-    // Remove leading and trailing whitespace from the line
-    stripwhite(line);
-
-    // If stripped line not blank
-    if (*line)
-    {
-      add_history(line);
-
-      Command cmd;
-      if (parse(line, &cmd) == 1)
-      {
-        // Just prints cmd
-        print_cmd(&cmd);
-	int pid;
-	int status;
-	Pgm * p = cmd.pgm;
-
-	
-		
-	while(p != NULL) {
-		char ** strs = p->pgmlist;	
-
-		if (strcmp(*strs, "exit") == 0) {
+int main(void) {
+	for (;;) {
+		char *line;
+		line = readline("> ");
+		if (line == NULL) {
 			return 0;
-		} else if (strcmp(*strs, "cd") == 0 && ++strs != NULL) {
-
-			if(chdir(*strs)) {
-				printf("directory: %s invalid\n", *strs);
-			}
-			//printf("changing dir to: %s\n", *strs);
-			goto next;
 		}
 
+		// Remove leading and trailing whitespace from the line
+		stripwhite(line);
 
-		int i = 0;
+		// If stripped line not blank
+		if (*line) {
+			add_history(line);
 
-		while (*(strs+i)) {	
-			printf("%s ", *(strs+i));
-			i++;
+			Command cmd;
+			if (parse(line, &cmd) == 1) {
+				// Just prints cmd
+				print_cmd(&cmd);
+				int pid;
+				int status;
+				Pgm *p = cmd.pgm;
+
+
+				while (p != NULL) {
+					char **strs = p->pgmlist;
+
+					if (strcmp(*strs, "exit") == 0) {
+						return 0;
+					} else if (strcmp(*strs, "cd") == 0 && ++strs != NULL) {
+						if (chdir(*strs)) {
+							printf("directory: %s invalid\n", *strs);
+						}
+						//printf("changing dir to: %s\n", *strs);
+						goto next;
+					}
+
+
+					int i = 0;
+
+					while (*(strs + i)) {
+						printf("%s ", *(strs + i));
+						i++;
+					}
+					printf("\n");
+
+
+					if ((pid = fork()) == 0) {
+						// redirect stdout
+						if (cmd.rstdout != NULL) {
+							int fd = open(cmd.rstdout, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+							dup2(fd, 1);
+							close(fd);
+						}
+
+						// redirect stderr
+						if (cmd.rstderr != NULL) {
+							int fd = open(cmd.rstdin, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+							dup2(fd, 2);
+							close(fd);
+						}
+
+						//redirect stdin
+						if (cmd.rstdin != NULL) {
+							int fd = open(cmd.rstdin, O_RDONLY);
+							dup2(fd, 0);
+							close(fd);
+						}
+
+						//execute command
+						if (*strs) {
+							execvp(*strs, strs);
+						}
+					} else {
+						wait(&status);
+						printf("status: %d\n", status);
+					}
+				next:
+					p = p->next;
+				}
+			} else {
+				printf("Parse ERROR\n");
+			}
 		}
-		printf("\n");
 
-
-		if( (pid = fork()) == 0) {
-
-			// redirect stdout
-			if (cmd.rstdout != NULL) {
-				int fd = open(cmd.rstdout, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-				dup2(fd, 1);
-				close(fd);
-			}
-			
-			// redirect stderr
-			if (cmd.rstderr !=NULL) {
-				int fd = open(cmd.rstdin, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-				dup2(fd, 2);
-				close(fd);
-			}
-
-			//redirect stdin
-			if (cmd.rstdin !=NULL) {
-				int fd = open(cmd.rstdin, O_RDONLY);
-				dup2(fd, 0);
-				close(fd);
-			}
-
-			//execute command
-			if (*strs) {
-				execvp(*strs, strs);
-			}
-		} else {
-			wait(&status);
-			printf("status: %d\n", status);
-		}
-next:
-		p = p->next;
+		// Clear memory
+		free(line);
 	}
-		
-      }
-      else
-      {
-        printf("Parse ERROR\n");
-      }
-    }
 
-    // Clear memory
-    free(line);
-  }
-
-  return 0;
+	return 0;
 }
 
 /*
@@ -144,43 +136,37 @@ next:
  *
  * Helper function, no need to change. Might be useful to study as inspiration.
  */
-static void print_cmd(Command *cmd_list)
-{
-  printf("------------------------------\n");
-  printf("Parse OK\n");
-  printf("stdin:      %s\n", cmd_list->rstdin ? cmd_list->rstdin : "<none>");
-  printf("stdout:     %s\n", cmd_list->rstdout ? cmd_list->rstdout : "<none>");
-  printf("background: %s\n", cmd_list->background ? "true" : "false");
-  printf("Pgms:\n");
-  print_pgm(cmd_list->pgm);
-  printf("------------------------------\n");
+static void print_cmd(Command *cmd_list) {
+	printf("------------------------------\n");
+	printf("Parse OK\n");
+	printf("stdin:      %s\n", cmd_list->rstdin ? cmd_list->rstdin : "<none>");
+	printf("stdout:     %s\n", cmd_list->rstdout ? cmd_list->rstdout : "<none>");
+	printf("background: %s\n", cmd_list->background ? "true" : "false");
+	printf("Pgms:\n");
+	print_pgm(cmd_list->pgm);
+	printf("------------------------------\n");
 }
 
 /* Print a (linked) list of Pgm:s.
  *
  * Helper function, no need to change. Might be useful to study as inpsiration.
  */
-static void print_pgm(Pgm *p)
-{
-  if (p == NULL)
-  {
-    return;
-  }
-  else
-  {
-    char **pl = p->pgmlist;
+static void print_pgm(Pgm *p) {
+	if (p == NULL) {
+		return;
+	} else {
+		char **pl = p->pgmlist;
 
-    /* The list is in reversed order so print
-     * it reversed to get right
-     */
-    print_pgm(p->next);
-    printf("            * [ ");
-    while (*pl)
-    {
-      printf("%s ", *pl++);
-    }
-    printf("]\n");
-  }
+		/* The list is in reversed order so print
+		 * it reversed to get right
+		 */
+		print_pgm(p->next);
+		printf("            * [ ");
+		while (*pl) {
+			printf("%s ", *pl++);
+		}
+		printf("]\n");
+	}
 }
 
 
@@ -188,25 +174,21 @@ static void print_pgm(Pgm *p)
  *
  * Helper function, no need to change.
  */
-void stripwhite(char *string)
-{
-  size_t i = 0;
+void stripwhite(char *string) {
+	size_t i = 0;
 
-  while (isspace(string[i]))
-  {
-    i++;
-  }
+	while (isspace(string[i])) {
+		i++;
+	}
 
-  if (i)
-  {
-    memmove(string, string + i, strlen(string + i) + 1);
-  }
+	if (i) {
+		memmove(string, string + i, strlen(string + i) + 1);
+	}
 
-  i = strlen(string) - 1;
-  while (i > 0 && isspace(string[i]))
-  {
-    i--;
-  }
+	i = strlen(string) - 1;
+	while (i > 0 && isspace(string[i])) {
+		i--;
+	}
 
-  string[++i] = '\0';
+	string[++i] = '\0';
 }
